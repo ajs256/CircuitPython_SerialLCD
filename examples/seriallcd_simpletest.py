67069c93ae4c082123de9135ed28ea43e53bd2ca
ajs256 <67526318+ajs256@users.noreply.github.com>
2021-01-10 18:14:07 -0800

    Add example

--- examples/seriallcd_simpletest.py
+++ examples/seriallcd_simpletest.py
@@ -1,3 +1,14 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: 2021 ajs256
 #
 # SPDX-License-Identifier: Unlicense
+
+import seriallcd
+import busio
+import board
+
+uart = busio.UART(board.TX, None, baudrate=9600)
+
+disp = seriallcd.Display(uart) # Create the display object, passing in the UART.
+
+disp.set_backlight(True) # Turn on the backlight.
+disp.print("Hello CircuitPython!") # Add some text to the display
